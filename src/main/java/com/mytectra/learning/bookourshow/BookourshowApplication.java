dbfa2af64660965e7574c0f0e522e580f6935446
Abhishek Tiwari <abhishekt5991@gmail.com>
2019-06-08 13:21:39 +0530

    Initial Push

---
+++ src/main/java/com/mytectra/learning/bookourshow/BookourshowApplication.java
@@ -0,0 +1,13 @@
+package com.mytectra.learning.bookourshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookourshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookourshowApplication.class, args);
+	}
+
+}
